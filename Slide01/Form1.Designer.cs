0afe1036da3700c23a45017e43612dcfef74131d
hvu26510 <hvu26510@gmail.com>
2024-03-06 15:04:21 +0700

    Add project files.

---
+++ Slide01/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Slide01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNhanVat = new System.Windows.Forms.ListBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbVungDat = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnShowCBB = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbNhanVat
+            // 
+            this.lbNhanVat.FormattingEnabled = true;
+            this.lbNhanVat.ItemHeight = 16;
+            this.lbNhanVat.Items.AddRange(new object[] {
+            "Yasuo",
+            "Yone",
+            "Irelia",
+            "Karma",
+            "Lessin"});
+            this.lbNhanVat.Location = new System.Drawing.Point(55, 33);
+            this.lbNhanVat.Name = "lbNhanVat";
+            this.lbNhanVat.Size = new System.Drawing.Size(311, 100);
+            this.lbNhanVat.TabIndex = 0;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(55, 166);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(173, 166);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Remove";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ListBox";
+            // 
+            // cbbVungDat
+            // 
+            this.cbbVungDat.FormattingEnabled = true;
+            this.cbbVungDat.Items.AddRange(new object[] {
+            "ionia",
+            "shurima",
+            "demacia"});
+            this.cbbVungDat.Location = new System.Drawing.Point(524, 33);
+            this.cbbVungDat.Name = "cbbVungDat";
+            this.cbbVungDat.Size = new System.Drawing.Size(291, 24);
+            this.cbbVungDat.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(521, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Combobox";
+            // 
+            // btnShowCBB
+            // 
+            this.btnShowCBB.Location = new System.Drawing.Point(524, 80);
+            this.btnShowCBB.Name = "btnShowCBB";
+            this.btnShowCBB.Size = new System.Drawing.Size(75, 23);
+            this.btnShowCBB.TabIndex = 7;
+            this.btnShowCBB.Text = "Show";
+            this.btnShowCBB.UseVisualStyleBackColor = true;
+            this.btnShowCBB.Click += new System.EventHandler(this.btnShowCBB_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(740, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(622, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 449);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnShowCBB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbbVungDat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lbNhanVat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbNhanVat;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbVungDat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnShowCBB;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Slide01/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slide01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(lbNhanVat.SelectedItem.ToString() + " da ha guc ban");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lbNhanVat.Items.Add("Kennen");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(lbNhanVat.SelectedIndex > -1)
+            {
+                lbNhanVat.Items.Remove(lbNhanVat.SelectedItem);
+            }
+        }
+
+        private void btnShowCBB_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cbbVungDat.SelectedItem.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cbbVungDat.Items.Add("Icathia");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (cbbVungDat.SelectedIndex > -1)
+            {
+                cbbVungDat.Items.Remove(cbbVungDat.SelectedItem);
+            }
+        }
+    }
+}
